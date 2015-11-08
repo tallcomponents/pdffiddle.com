b61fd9a6b3ceb67d4b08a0374951c0f4c71ef059
Frank Rem <frank@tallcomponents.com>
2015-11-08 15:05:06 +0100

    Initial import

---
+++ website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PdfFiddle.Startup))]
+namespace PdfFiddle
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

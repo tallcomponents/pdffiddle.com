cc6dd5f105ae935b6d1d60418f62371a872b949c
Frank Rem <frank@tallcomponents.com>
2015-11-08 16:29:25 +0100

    First version that renders a PDF page from operators/operands.
    A 4:8 column design. Left column for entering PDF operator as text.
    Right-column displays rendered PDF.

--- website/Controllers/HomeController.cs
+++ website/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
+using TallComponents.PDF.Base.DocumentStructure;
+using TallComponents.PDF.Base.Navigation;
+using Base = TallComponents.PDF.Base;
 
 namespace PdfFiddle.Controllers
 {
@@ -25,6 +29,33 @@ namespace PdfFiddle.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult CreatePdf(string content)
+        {
+            // create pdf
+            Document pdf = new Document();
+            Pages pages = new Pages();
+            pdf.Catalog.Pages = pages;
+            Page page = new Page(200, 200);
+            pages.Add(page);
+            Base.Graphics.Content stream = new Base.Graphics.Content(content);
+            page.Contents = stream;
+
+            ViewerPreferences prefs = new ViewerPreferences();
+            Base.Array destination = new Base.Array();
+            destination.Add(new Base.IndirectReference(page));
+            destination.Add(new Base.Name("Fit"));
+            pdf.Catalog.OpenAction = destination;
+
+            Guid guid = Guid.NewGuid();
+            string path = Server.MapPath(string.Format("~/temp/{0}.pdf", guid));
+            using (FileStream file = new FileStream(path, FileMode.Create, FileAccess.Write))
+            {
+                pdf.Write(file);
+            }
+
+            return Content(guid.ToString());
         }
     }
 }
